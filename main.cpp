f944e65d4720d304abf878016f1329b42408ec72
Sergey Masuryan <smasuryan@plgn.ru>
2016-11-09 11:06:38 +0500

    binary search: loop bug fixed

--- main.cpp
+++ main.cpp
@@ -17,19 +17,34 @@ int bin_search(int a[], int v, int l, int r)
 template <typename T, typename  Type>
 int bin_search_containers(T container, Type _value)
 {
-  size_t size = container.size();
-  int middle = 0;
-  Type l = container.front();
-  Type r = container.back();
+    Type l = 0;
+    Type r = container.size();
+    int middle = (l + r) / 2;
 
-  int z = r - l;
+    while ( middle >= 1 ) {
+        middle = (l + r) / 2;
 
-  while ( z >= 1 ) {
-     middle = size / 2;
-     if (_value == container.at(middle)) return middle;
-     if (_value < container.at(middle)) r = middle - 1; else l = middle + 1;
-    }
-  return -1;
+     /*
+     * для такого случия vector<int> vec{1, 1, 1, 3, 6, 8, 8, 10, 10, 12};
+     * бывают колизии и цикл продолжается бесконечно, вот по этому и нужна
+     * проверка  'f(r > l)'
+     */
+        if(r > l) {
+            if (_value == container.at(middle)) {
+                return middle;
+              }
+            if (_value < container.at(middle)) {
+                r = middle - 1;
+              }
+            else {
+                l = middle + 1;
+              }
+          }
+        else {
+            return -1;
+          }
+      }
+    return -1;
 }
 
 int main()
@@ -39,30 +54,31 @@ int main()
 
       my_boost_int_Rnd rnd;
       vector<int> vec;
-      int a[10];
+      //int a[10];
 
-      for (int var = 0; var < 10; ++var) {
-         vec.push_back(rnd.int_boost_rnd(1, 15));
-         a[var] = rnd.int_boost_rnd(1, 15);
+      for (int var = 0; var < 25; ++var) {
+          vec.push_back(rnd.int_boost_rnd(1, 20));
+          //a[var] = rnd.int_boost_rnd(1, 15);
         }
 
-      sort(begin(a), end(a));
+      //sort(begin(a), end(a));
       sort(vec.begin(), vec.end());
+
 
       for (auto j : vec)
               cout << j << " ";
       cout << endl;
       int s = rnd.int_boost_rnd(1,15);
-      int x = bin_search_containers(vec, 5);
+      int x = bin_search_containers(vec, s);
 
       if(x < 0)
         cout << s << " not founded" << endl;
       else
-        cout  << s << " at a[" << x << "]" << endl;
+        cout << s << " at a[" << x << "]" << endl;
 
 
-
-     /* for (auto j : a)
+/*
+         for (auto j : a)
         cout << j << " ";
       cout << endl;
 
