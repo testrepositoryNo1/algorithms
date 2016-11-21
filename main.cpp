242848531329cc28513cf9f47194bd140814aed2
Sergey Masuryan <smasuryan@plgn.ru>
2016-11-21 14:42:15 +0500

    SORT:

--- main.cpp
+++ main.cpp
@@ -2,161 +2,51 @@
 
 using namespace std;
 
-static int c = 0;
-
-int f(int n)
+template<typename  _Type>
+void my_sort(vector<_Type> &v)
 {
-  ++c;
-  while (n) {
-      cout << n << endl;
-   return f(n-1);
+    _Type temp = 0;
+
+    for(size_t i = 0; i < v.size(); ++i) {
+        for(size_t j = 0; j < v.size(); ++j) {
+            if(v.at(i) > v.at(j)) {
+                temp = v.at(j);
+                v.at(j) = v.at(i);
+                v.at(i) = temp;
+            }
+        }
     }
-  return -1;
 }
 
-
-template <class ForwardIterator>
-  ForwardIterator _max_element ( ForwardIterator first, ForwardIterator last )
+void gen(vector<int> &v, size_t _size)
 {
-  if (first == last) return last;
-  ForwardIterator largest = first;
-
-  while (++first != last)
-    if (*largest < *first)    // or: if (comp(*largest,*first)) for version (2)
-      largest = first;
-  return largest;
+    my_boost_int_Rnd rnd;
+    for (size_t i = 0; i < _size; ++i)
+        v.push_back(rnd.int_boost_rnd(1, 100));
 }
 
-
-/*
-*
-* рекурсивный поиск максимума
-*
-*/
-int max(int a[], int l, int r)
+int main ()
 {
-  if (l == r) return a[l];
-  int m = (l + r) / 2;
-  int u = max(a, l , m);
-  int v = max(a, m + 1, r);
-  if (u > v) return u; else return v;
-}
-
-
-
-
-// Наша структура
-struct node
-{
-    int info;     // Информационное поле
-    node *l, *r;  // Левая и Правая часть дерева
-};
-
-node *tree = 0; //Объявляем переменную, тип которой структура Дерево
-
-/*--- ФУНКЦИЯ ЗАПИСИ ЭЛЕМЕНТА В БИНАРНОЕ ДЕРЕВО ---*/
-void push(int a, node **t)
-{
-    if ((*t) == 0) {              // Если дерева не существует
-        (*t) = new node;          // Выделяем память
-        (*t)->info = a;           // Кладем в выделенное место аргумент a
-        (*t)->l = (*t)->r = 0;    // Очищаем память для следующего роста
-        return;                 // Заложили семечко, выходим
-    }
-       //Дерево есть
-        if (a > (*t)->info)
-          push(a, &(*t)->r);      // Если аргумент а больше чем текущий элемент,
-                                  // кладем его вправо
-        else push(a, &(*t)->l);   // Иначе кладем его влево
-}
-
-/*--- ФУНКЦИЯ ОТОБРАЖЕНИЯ ДЕРЕВА НА ЭКРАНЕ ---*/
-void print (node *t, int u)
-{
-    if (t == 0) return;       //Если дерево пустое, то отображать нечего, выходим
-    else {
-        print(t->l, ++u);     //С помощью рекурсивного посещаем левое поддерево
-    for (int i = 0; i < u; ++i)
-      cout << "-";
-    cout << t->info << endl;  //И показываем элемент
-    u--;
-      }
-    print(t->r, ++u);         // С помощью рекурсии посещаем правое поддерево
-    delete t;                 // Освобождаем выделенную память
-}
-
-int myrandom () { return rand()%100;}
-
-int main()
-{
-      boost::chrono::milliseconds start(clock());
+    setlocale(0, "");
+    srand(time(0));
+    srand(rand());
+    ostream_iterator<int> out(cout, "\n");
+    boost::chrono::milliseconds start(clock());
 //----------------------------------------------------------------
 
+    vector<int> vec;
 
-      vector<int> vec;
-      my_boost_int_Rnd  rnd;
+    gen(vec, 20);
+
+    my_sort(vec);
 
 
-
-
-      int n = 0; //Количество элементов
-      int s = 0; //Число, передаваемое в дерево
-
-      while (1) {
-          cout << "введите количество элементов  ";
-          cin >> n; //Вводим количество элементов
-
-          //for (int i=0; i < n; ++i) {
-
-          cout << "массив: ";
-              while (vec.size() < n) {
-                  s = rnd.int_boost_rnd(1, 25);
-                  vec.push_back(s);
-                  cout << s <<  " "; //Считываем элемент за элементом
-                }
-              cout << endl;
-              //sort(vec.begin(), vec.end());
-              //unique(vec.begin(), vec.end());
-              //random_shuffle(vec.begin(), vec.end());
-
-              for (auto a: vec)
-                push(a, &tree); //И каждый кладем в дерево
-            //}
-          cout << "ваше дерево\n";
-          print(tree, 0);
-          cout << endl;
-          vec.clear();
-  }
+    for(auto a : vec)
+        cout << a << " ";
+    cout << endl;
 
 
 
-     /* auto it = vec.begin();
-
-      for (; it != vec.end(); ++it)
-        if(*it > max)
-        max = *it;*/
-
- /*     auto pos1 = vec.begin();
-      thread th([&pos1,&vec](){
-          auto tmp_pos = _max_element(vec.begin(), (vec.begin() + vec.size() / 2));
-          pos1 = tmp_pos;
-        });
-
-*/
-
-/*
-      auto  pos1 = _max_element(vec.begin(), (vec.begin() + vec.size() / 2));
-      //int   max1 = *pos1;
-
-      auto  pos2 = _max_element((vec.begin() + vec.size() / 2), vec.end());
-      //int   max2 = *pos2;
-
-      //th.join();
-      if(*pos1 > *pos2)
-        cout << *pos1 << endl;
-      else
-        cout << *pos2 << endl;
-      */
 
 //---------------------------------------------------------------
       boost::chrono::milliseconds end(clock());
