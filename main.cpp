db1f8b75b6e9d6a2755ec400d401a3f708598256
Sergey Masuryan <smasuryan@plgn.ru>
2016-12-08 15:59:29 +0500

    MERGE SORT: fixed some bugs, and optimized and improoved some moments

--- main.cpp
+++ main.cpp
@@ -2,166 +2,12 @@
 
 using namespace std;
 
-size_t c = 0;
-
-void gen(vector<int> &v, size_t _size)
-{
-    my_boost_int_Rnd rnd;
-    for (size_t i = 0; i < _size; ++i) {
-        v.push_back(rnd.int_boost_rnd());
-        }
-}
-
-
-template <typename  _Type>
-void exch(_Type &a, _Type &b)
-{
-    _Type temp;
-    temp = a;
-    a = b;
-    b = temp;
-}
-
-
-template <typename  Type>
-void bubble_sort(vector<Type> &v)
-{
-    for(size_t i = 0; i < v.size(); ++i) {
-            for(size_t j = 0; j < v.size(); ++j) {
-                    /* < - сортировка по возростанию, > - сортировка по убыванию*/
-                    if(v.at(i) < v.at(j)) {
-                            exch(v.at(i), v.at(j));
-                            ++c;
-                        }
-                }
-            ++c;
-        }
-}
-
-
-template <typename  Type>
-void insertion_sort(vector<Type> &v)
-{
-    Type min = v.front(), pos = 0;
-
-    for(size_t i = 0; i < v.size(); ++i) {
-            size_t j = i;
-            min = v.at(j);
-            while (j < v.size()) {
-                    /* < - сортировка по возростанию, > - сортировка по убыванию*/
-                    if (v.at(j) < min){
-                            min = v.at(j);
-                            pos = j;
-                        }
-                    ++j;
-                    ++c;
-                }
-            exch(v.at(i), v.at(pos));
-            j = 0;
-            ++c;
-        }
-}
-
-
-template <typename Type>
-void shellsort(vector<Type> &vec)
-{
-    size_t h = 1;
-    size_t r = vec.size();
-    size_t l = 0;
-
-    for ( ; h < (r - l) / 9; h = (3 * h) + 1);
-    for( ; h > 0; h /= 3)
-        for(size_t i = l + h; i < r; ++i) {
-                size_t j = i;
-                Type v = vec.at(i);
-                while (j >= l + h && v < vec.at(j - h)) {
-                        vec.at(j) = vec.at(j - h);
-                        j -= h;
-                    }
-                vec.at(j) = v;
-            }
-}
-
-
-int partition_ (int *a, int p, int r)
-{
-    int x = *(a+r);
-    int i = p - 1;
-    int j;
-    int tmp;
-    for (j = p; j < r; j++)
-        {
-            if (*(a+j) <= x)
-                {
-                    i++;
-                    tmp = *(a+i);
-                    *(a+i) = *(a+j);
-                    *(a+j) = tmp;
-                }
-        }
-    tmp = *(a+r);
-    *(a+r) = *(a+i+1);
-    *(a+i+1) = tmp;
-    return i + 1;
-}
-
-
-
-/*template <typename Type>
-void quicksort(int* a, int l, int r)
-{
-    if (r <= 1) return;
-    int i = partition_(a, l, r);
-    quicksort(a, l, i - 1);
-    quicksort(a, i + 1, r);
-}*/
-
-
-/*void quicksort (int *a, int p, int r)
-{
-    int q;
-    if (p < r)   {
-            q = partition_ (a, p, r);
-            quicksort (a, p, q-1);
-            quicksort (a, q+1, r);
-        }
-}*/
-
-
-/*template <typename Type>
-void quicksort(vector<Type> &v)
-{
-    Type l = *v.begin();
-    Type r = *v.rbegin();
-
-    if (r <= 1) return;
-    auto i = partition(v, l, r);
-    quicksort(v, l, i - 1);
-    quicksort(v, i + 1, r);
-    qsort();
-
-}
-*/
-
-
-/*void quicksort (int *a, int p, int r)
- {
-    int q;
-    if (p < r)   {
-            q = partition_ (a, p, r);
-            quicksort (a, p, q-1);
-            quicksort (a, q+1, r);
-        }
-}
-*/
-
-
 /*
    merge sort algorithm for two sorted files
    algorithm will not work correctly, if source files are not sorted
+   modified (improved)
 */
-void merger(string first_file_name, string second_file_name)
+void merger_(string first_file_name, string second_file_name)
 {
     vector<int> first_part, second_part;;
     ifstream fin1, fin2;
@@ -182,21 +28,30 @@ void merger(string first_file_name, string second_file_name)
             second_part.push_back(b);
         }
 
-    first_part.pop_back();  /* без этого не обойтись */
-    second_part.pop_back(); /* без этого не обойтись */
+    first_part.pop_back();  /* без этого не обойтись, удаляем лишнее*/
+    second_part.pop_back(); /* без этого не обойтись, удаляем лишнее */
 
     auto iter1 = first_part.begin();
     auto iter2 = second_part.begin();
 
     /*--- realisation of algorithm ---*/
-    for
-            (size_t i = 0; i < first_part.size() * 2; ++i){
+    for (size_t i = 0; i < first_part.size() * 2; ++i){
             if (*iter1 <= *iter2) {
                     fout << *iter1 << "\n";
-                    //vec.push_back(*iter1); // на случай если захочется записать в массив
-                                             //   (но лучше в список)
+                    //vec.push_back(*iter1);
                     if (iter1 == first_part.end() - 1) {
-                            *iter1 = *iter2 + 1; /* без этого не обойтись ()*/
+                            /* всего лишь надо было понять,
+                               что если достигли конца в одном файле,
+                               а во втором еще есть элементы то эти элементы
+                               из второго файла просто надо добавить в конец выходного файла
+                               (потому как они уже отсортированы,
+                               и больше максимального элемента первого файла)!
+                             */
+                            while (iter2 != second_part.end()) {
+                                    fout << *iter2 << "\n";
+                                    //vec.push_back(*iter2);
+                                    ++iter2;
+                                } break;
                         }
                     else {
                             ++iter1;
@@ -204,10 +59,14 @@ void merger(string first_file_name, string second_file_name)
                 }
             else if (*iter1 >= *iter2) {
                     fout << *iter2 << "\n";
-                    //vec.push_back(*iter2); // на случай если захочется записать в массив
-                                             //   (но лучше в список)
+                    //vec.push_back(*iter2);
                     if (iter2 == second_part.end() - 1) {
-                            *iter2 = *iter1 + 1; /* без этого не обойтись ()*/
+                            /* см. обяснеие выше стоящего цыкла 'while' */
+                            while (iter1 != first_part.end()) {
+                                    fout << *iter1 << "\n";
+                                    //vec.push_back(*iter1);
+                                    ++iter1;
+                                } break;
                         }
                     else {
                             ++iter2;
@@ -216,103 +75,9 @@ void merger(string first_file_name, string second_file_name)
         }
     /*---*---*/
     fout.close();
-
-    /* some boost tricks */
-    size_t file_size = boost::filesystem::file_size("merged_file");
-    if (file_size > 50000000){
-            boost::filesystem::rename("merged_file", "merged_file_is_too_big_don't_open_it");
-        }
 }
 
 
-/*
-   this function generate tow files,
-   which contains sorted integer values
-*/
-void big_files_generator()
-{
-    vector<int> vec, first_part, second_part;
-    gen(vec, 1000000);
-
-    auto iter  = vec.begin();
-    auto iter2 = vec.begin() + vec.size() / 2;
-
-    for (; iter != (vec.begin() + vec.size() / 2); ++iter, ++iter2) {
-            first_part.push_back(*iter);
-            second_part.push_back(*iter2);
-        }
-
-    sort(first_part.begin(), first_part.end());
-    sort(second_part.begin(), second_part.end());
-
-
-    iter  = first_part.begin();
-    iter2 = second_part.begin();
-
-    thread thr([&first_part, &iter]()
-    {
-            ofstream fout;
-            fout.open("first_part", ios::out | ios::app);
-            for ( ; iter != first_part.end(); ++ iter) {
-                    fout << *iter << "\n";
-                }
-            fout.close();
-
-        });
-
-    ofstream fout;
-    fout.open("second_part", ios::out | ios::app);
-    for ( ; iter2 != second_part.end(); ++ iter2) {
-            fout << *iter2 << "\n";
-        }
-
-    thr.join();
-}
-
-
-/*
-   this function generate tow files,
-   which contains sorted integer values
-*/
-void small_files_generator()
-{
-    vector<int> first_part,
-                second_part;
-
-    for (size_t i = 0; i < 200; ++i){
-            if (i%2 == 0)
-                first_part.push_back(i);
-            else
-                second_part.push_back(i);
-        }
-
-
-    sort(first_part.begin(), first_part.end());
-    sort(second_part.begin(), second_part.end());
-
-
-    auto iter  = first_part.begin();
-    auto iter2 = second_part.begin();
-
-    thread thr([&first_part, &iter]()
-    {
-            ofstream fout;
-            fout.open("first_part", ios::out);
-            for ( ; iter != first_part.end(); ++ iter) {
-                    fout << *iter << "\n";
-                }
-            fout.close();
-
-        });
-
-    ofstream fout;
-    fout.open("second_part", ios::out);
-    for ( ; iter2 != second_part.end(); ++ iter2) {
-            fout << *iter2 << "\n";
-        }
-
-    thr.join();
-}
 
 
 int main ()
@@ -321,10 +86,11 @@ int main ()
     boost::chrono::milliseconds start(clock());
 //----------------------------------------------------------------
 
+    big_files_generator(50, -50, 50);
     string first_file_name  = "first_part",
            second_file_name = "second_part";
 
-    merger(first_file_name, second_file_name);
+    merger_(first_file_name, second_file_name); /* modified */
 
 
 //---------------------------------------------------------------
--- main.hpp
+++ main.hpp
@@ -1,6 +1,7 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 #include "stdfx.hpp"
+using namespace std;
 
 #define DATA_TYPE boost::int64_t  /* for boost spreadsort*/
 
@@ -176,5 +177,326 @@ T Stack<T>::top() const
     return elems.back(); // Возврат копии последнего элемента
 }
 */
+
+
+size_t c = 0;
+
+void gen(vector<int> &v, size_t _size)
+{
+    my_boost_int_Rnd rnd;
+    for (size_t i = 0; i < _size; ++i) {
+        v.push_back(rnd.int_boost_rnd());
+        }
+}
+
+void gen(vector<int> &v, size_t _size, int from, int to)
+{
+    my_boost_int_Rnd rnd;
+    for (size_t i = 0; i < _size; ++i) {
+        v.push_back(rnd.int_boost_rnd(from, to));
+        }
+}
+
+
+template <typename  _Type>
+void exch(_Type &a, _Type &b)
+{
+    _Type temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+
+template <typename  Type>
+void bubble_sort(vector<Type> &v)
+{
+    for(size_t i = 0; i < v.size(); ++i) {
+            for(size_t j = 0; j < v.size(); ++j) {
+                    /* < - сортировка по возростанию, > - сортировка по убыванию*/
+                    if(v.at(i) < v.at(j)) {
+                            exch(v.at(i), v.at(j));
+                            ++c;
+                        }
+                }
+            ++c;
+        }
+}
+
+
+template <typename  Type>
+void insertion_sort(vector<Type> &v)
+{
+    Type min = v.front(), pos = 0;
+
+    for(size_t i = 0; i < v.size(); ++i) {
+            size_t j = i;
+            min = v.at(j);
+            while (j < v.size()) {
+                    /* < - сортировка по возростанию, > - сортировка по убыванию*/
+                    if (v.at(j) < min){
+                            min = v.at(j);
+                            pos = j;
+                        }
+                    ++j;
+                    ++c;
+                }
+            exch(v.at(i), v.at(pos));
+            j = 0;
+            ++c;
+        }
+}
+
+
+template <typename Type>
+void shellsort(vector<Type> &vec)
+{
+    size_t h = 1;
+    size_t r = vec.size();
+    size_t l = 0;
+
+    for ( ; h < (r - l) / 9; h = (3 * h) + 1);
+    for( ; h > 0; h /= 3)
+        for(size_t i = l + h; i < r; ++i) {
+                size_t j = i;
+                Type v = vec.at(i);
+                while (j >= l + h && v < vec.at(j - h)) {
+                        vec.at(j) = vec.at(j - h);
+                        j -= h;
+                    }
+                vec.at(j) = v;
+            }
+}
+
+
+int partition_ (int *a, int p, int r)
+{
+    int x = *(a+r);
+    int i = p - 1;
+    int j;
+    int tmp;
+    for (j = p; j < r; j++)
+        {
+            if (*(a+j) <= x)
+                {
+                    i++;
+                    tmp = *(a+i);
+                    *(a+i) = *(a+j);
+                    *(a+j) = tmp;
+                }
+        }
+    tmp = *(a+r);
+    *(a+r) = *(a+i+1);
+    *(a+i+1) = tmp;
+    return i + 1;
+}
+
+
+
+/*template <typename Type>
+void quicksort(int* a, int l, int r)
+{
+    if (r <= 1) return;
+    int i = partition_(a, l, r);
+    quicksort(a, l, i - 1);
+    quicksort(a, i + 1, r);
+}*/
+
+
+/*void quicksort (int *a, int p, int r)
+{
+    int q;
+    if (p < r)   {
+            q = partition_ (a, p, r);
+            quicksort (a, p, q-1);
+            quicksort (a, q+1, r);
+        }
+}*/
+
+
+/*template <typename Type>
+void quicksort(vector<Type> &v)
+{
+    Type l = *v.begin();
+    Type r = *v.rbegin();
+
+    if (r <= 1) return;
+    auto i = partition(v, l, r);
+    quicksort(v, l, i - 1);
+    quicksort(v, i + 1, r);
+    qsort();
+
+}
+*/
+
+
+/*void quicksort (int *a, int p, int r)
+ {
+    int q;
+    if (p < r)   {
+            q = partition_ (a, p, r);
+            quicksort (a, p, q-1);
+            quicksort (a, q+1, r);
+        }
+}
+*/
+
+
+/*
+   merge sort algorithm for two sorted files
+   algorithm will not work correctly, if source files are not sorted
+*/
+void merger(string first_file_name, string second_file_name)
+{
+    vector<int> first_part, second_part;;
+    ifstream fin1, fin2;
+    ofstream fout("merged_file", ios::out);
+
+
+    fin1.open(first_file_name, ios::in);
+    fin2.open(second_file_name, ios::in);
+
+    int a   = 0,
+        b   = 0;
+
+    /* считивание из файлов */
+    while (fin1 && fin2) {
+            fin1 >> a;
+            first_part.push_back(a);
+            fin2 >> b;
+            second_part.push_back(b);
+        }
+
+    first_part.pop_back();  /* без этого не обойтись */
+    second_part.pop_back(); /* без этого не обойтись */
+
+    auto iter1 = first_part.begin();
+    auto iter2 = second_part.begin();
+
+    /*--- realisation of algorithm ---*/
+    for
+            (size_t i = 0; i < first_part.size() * 2; ++i){
+            if (*iter1 <= *iter2) {
+                    fout << *iter1 << "\n";
+                    //vec.push_back(*iter1); // на случай если захочется записать в массив
+                                             //   (но лучше в список)
+                    if (iter1 == first_part.end() - 1) {
+                            *iter1 = *iter2 + 1; /* без этого не обойтись ()*/
+                        }
+                    else {
+                            ++iter1;
+                        }
+                }
+            else if (*iter1 >= *iter2) {
+                    fout << *iter2 << "\n";
+                    //vec.push_back(*iter2); // на случай если захочется записать в массив
+                                             //   (но лучше в список)
+                    if (iter2 == second_part.end() - 1) {
+                            *iter2 = *iter1 + 1; /* без этого не обойтись ()*/
+                        }
+                    else {
+                            ++iter2;
+                        }
+                }
+        }
+    /*---*---*/
+    fout.close();
+
+    /* some boost tricks */
+    size_t file_size = boost::filesystem::file_size("merged_file");
+    if (file_size > 50000000){
+            boost::filesystem::rename("merged_file", "merged_file_is_too_big_don't_open_it");
+        }
+}
+
+
+/*
+   this function generate tow files,
+   which contains sorted integer values
+*/
+void big_files_generator(size_t size, int from, int to)
+{
+    vector<int> vec, first_part, second_part;
+    gen(vec, (size * 2), from, to);
+
+    auto iter  = vec.begin();
+    auto iter2 = vec.begin() + vec.size() / 2;
+
+    for (; iter != (vec.begin() + vec.size() / 2); ++iter, ++iter2) {
+            first_part.push_back(*iter);
+            second_part.push_back(*iter2);
+        }
+
+    sort(first_part.begin(), first_part.end());
+    sort(second_part.begin(), second_part.end());
+
+
+    iter  = first_part.begin();
+    iter2 = second_part.begin();
+
+    thread thr([&first_part, &iter]()
+    {
+            ofstream fout;
+            fout.open("first_part", ios::out);
+            for ( ; iter != first_part.end(); ++ iter) {
+                    fout << *iter << "\n";
+                }
+            fout.close();
+
+        });
+
+    ofstream fout;
+    fout.open("second_part", ios::out);
+    for ( ; iter2 != second_part.end(); ++ iter2) {
+            fout << *iter2 << "\n";
+        }
+
+    thr.join();
+}
+
+
+/*
+   this function generate tow files,
+   which contains sorted integer values
+*/
+void small_files_generator()
+{
+    vector<int> first_part,
+                second_part;
+
+    for (size_t i = 0; i < 200; ++i){
+            if (i%2 == 0)
+                first_part.push_back(i);
+            else
+                second_part.push_back(i);
+        }
+
+
+    sort(first_part.begin(), first_part.end());
+    sort(second_part.begin(), second_part.end());
+
+
+    auto iter  = first_part.begin();
+    auto iter2 = second_part.begin();
+
+    thread thr([&first_part, &iter]()
+    {
+            ofstream fout;
+            fout.open("first_part", ios::out);
+            for ( ; iter != first_part.end(); ++ iter) {
+                    fout << *iter << "\n";
+                }
+            fout.close();
+
+        });
+
+    ofstream fout;
+    fout.open("second_part", ios::out);
+    for ( ; iter2 != second_part.end(); ++ iter2) {
+            fout << *iter2 << "\n";
+        }
+
+    thr.join();
+}
 
 #endif // MAIN_HPP
